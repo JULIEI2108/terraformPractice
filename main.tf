ba5508c5d5f3a130a82ad60b3a7dcc7d40f00425
jinjinlei2108 <Mofaxvetu1>
2023-05-28 13:48:28 +1000

    day 6 practice

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ data "aws_region" "current" {}
 
 # Terraform Data Block - Lookup Ubuntu 16.04
 data "aws_ami" "ubuntu_16_04" {
-most_recent = true
-filter {
-name = "name"
-values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-}
-owners = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
 }
 
 #Define the VPC 
@@ -132,8 +132,26 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
 
   subnet_id              = aws_subnet.public_subnets["public_subnet_1"].id
-  vpc_security_group_ids = ["sg-0a6cfbf6b782782fa"]
+  vpc_security_group_ids = [aws_security_group.ingress-ssh.id, aws_security_group.vpc-ping.id, aws_security_group.vpc_web.id]
+  key_name               = aws_key_pair.generated.key_name
+  connection {
+    user        = "ubuntu"
+    private_key = tls_private_key.generated.private_key_pem
+    host        = self.public_ip
+  }
 
+  provisioner "local-exec" {
+    command = "chmod 600 ${local_file.private_key_pem.filename}"
+
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo rm -rf /tmp",
+      "sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp",
+      "sudo sh /tmp/assets/setup-web.sh",
+    ]
+  }
   tags = {
     "Terraform" = "true"
   }
@@ -152,3 +170,87 @@ resource "aws_subnet" "variables-subnet" {
 
 }
 
+resource "aws_security_group" "ingress-ssh" {
+  name   = "allow-all-ssh"
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+# create Security Group - Web Traffic
+resource "aws_security_group" "vpc_web" {
+  name        = "vpc-web-${terraform.workspace}"
+  vpc_id      = aws_vpc.vpc.id
+  description = "Web Traffic"
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "vpc-ping" {
+  name        = "vpc-ping"
+  vpc_id      = aws_vpc.vpc.id
+  description = "ICMP for Ping Access"
+  ingress {
+    description = "Allow ICMP Traffic"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outboun"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+
+}
+
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "MyAWSKey.pem"
+
+}
+
+resource "aws_key_pair" "generated" {
+  key_name   = "MyAWSKEY"
+  public_key = tls_private_key.generated.public_key_openssh
+
+  lifecycle {
+    ignore_changes = [key_name]
+  }
+}
--- terraform.tf
+++ terraform.tf
@@ -5,5 +5,25 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = ">= 2.1.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.3"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+
   }
 }
