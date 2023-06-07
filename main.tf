87c01df617f4c1b8f73a8e7ce4981e1bc251e2ec
jinjinlei2108 <jinjinlei2108@hotmail.com>
2023-06-07 20:42:25 +1000

    module study

--- main.tf
+++ main.tf
@@ -254,3 +254,63 @@ resource "aws_key_pair" "generated" {
     ignore_changes = [key_name]
   }
 }
+
+module "server" {
+  source    = "./modules/server"
+  ami       = data.aws_ami.ubuntu_16_04.id
+  subnet_id = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id,
+    aws_security_group.ingress-ssh.id,
+  aws_security_group.vpc_web.id]
+}
+
+output "public_ip" {
+  value = module.server.public_ip
+}
+
+output "public_dns" {
+  value = module.server.public_dns
+
+}
+output "size" {
+  value = module.server.size
+
+}
+
+
+module "server_subnet_1" {
+  source      = "./modules/web_server"
+  ami         = data.aws_ami.ubuntu_16_04.id
+  key_name    = aws_key_pair.generated.key_name
+  user        = "ubuntu"
+  private_key = tls_private_key.generated.private_key_pem
+  subnet_id   = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id,
+    aws_security_group.ingress-ssh.id,
+  aws_security_group.vpc_web.id]
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "4.11.0"
+
+
+  name = "myasg"
+
+  vpc_zone_identifier = [aws_subnet.private_subnets["private_subnet_1"].id,
+  aws_subnet.private_subnets["private_subnet_2"].id,
+  aws_subnet.private_subnets["private_subnet_3"].id]
+  min_size = 0
+  max_size = 1
+  desired_capacity = 1
+
+  # launch template
+  use_lt = true
+  create_lt = true
+  image_id = data.aws_ami.ubuntu_16_04.id
+  instance_type = "t3.micro"
+
+  tags_as_map = {
+    Name = "Web EC2 Server 2"
+  }
+}
